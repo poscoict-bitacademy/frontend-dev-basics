bb7407d0d64888d1e8397bb065f4fcf96544658f
poscoict-bitacademy <poscoict.bitacademy@gmail.com>
2022-02-25 15:54:49 +0900

    ajax #2

--- ch08/src/main/java/com/poscoict/ch08/controller/api/ApiController.java
+++ ch08/src/main/java/com/poscoict/ch08/controller/api/ApiController.java
@@ -4,6 +4,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.poscoict.ch08.controller.vo.GuestbookVo;
+
 @Controller
 @RequestMapping("/api")
 public class ApiController {
@@ -13,4 +15,22 @@ public class ApiController {
 	public String text() {
 		return "Text 데이터";
 	}
+	
+	@ResponseBody
+	@RequestMapping("/html")
+	public String html() {
+		return "<h1>AJAX 연습</h1><p>HTML 데이터</p>";
+	}
+	
+	@ResponseBody
+	@RequestMapping("/json")
+	public Object json() {
+		GuestbookVo vo = new GuestbookVo();
+		vo.setNo(1L);
+		vo.setName("둘리");
+		vo.setMessage("호이~");
+		
+		return vo;
+	}
+	
 }
