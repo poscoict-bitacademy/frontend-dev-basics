0b66372047a8663beccd666e0061ad1cdda7362b
poscoict-bitacademy <poscoict.bitacademy@gmail.com>
2022-02-25 16:56:31 +0900

    ajax #3

--- ch08/src/main/java/com/poscoict/ch08/controller/api/ApiController.java
+++ ch08/src/main/java/com/poscoict/ch08/controller/api/ApiController.java
@@ -2,8 +2,10 @@ package com.poscoict.ch08.controller.api;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.poscoict.ch08.controller.dto.JsonResult;
 import com.poscoict.ch08.controller.vo.GuestbookVo;
 
 @Controller
@@ -23,14 +25,14 @@ public class ApiController {
 	}
 	
 	@ResponseBody
-	@RequestMapping("/json")
-	public Object json() {
+	@RequestMapping(value="/json", method=RequestMethod.GET)
+	public JsonResult json() {
 		GuestbookVo vo = new GuestbookVo();
 		vo.setNo(1L);
 		vo.setName("둘리");
 		vo.setMessage("호이~");
 		
-		return vo;
+		return JsonResult.success(vo);
 	}
 	
 }
--- ch08/src/main/java/com/poscoict/ch08/controller/test/ApiController.java
+++ ch08/src/main/java/com/poscoict/ch08/controller/test/ApiController.java
@@ -16,5 +16,10 @@ public class ApiController {
 	public String html() {
 		return "html";
 	}
+
+	@RequestMapping("/json01")
+	public String json01() {
+		return "json01";
+	}
 	
 }
