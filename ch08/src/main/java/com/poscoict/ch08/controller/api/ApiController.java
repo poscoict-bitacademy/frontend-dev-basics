153e71ae2aa103acdfaac293f56e4bf3182c9136
poscoict-bitacademy <poscoict.bitacademy@gmail.com>
2022-02-28 14:59:58 +0900

    ajax #9-xml지원

--- ch08/src/main/java/com/poscoict/ch08/controller/api/ApiController.java
+++ ch08/src/main/java/com/poscoict/ch08/controller/api/ApiController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.poscoict.ch08.controller.dto.JsonResult;
+import com.poscoict.ch08.controller.dto.XmlResult;
 import com.poscoict.ch08.controller.vo.GuestbookVo;
 
 @Controller
@@ -24,10 +25,21 @@ public class ApiController {
 	public String html() {
 		return "<h1>AJAX 연습</h1><p>HTML 데이터</p>";
 	}
-	
+
+	@ResponseBody
+	@RequestMapping("/xml")
+	public Object xml() {
+		XmlResult.GuestbookVo vo = new XmlResult.GuestbookVo();
+		vo.setNo(1L);
+		vo.setName("둘리");
+		vo.setMessage("호이~");
+		
+		return XmlResult.success(vo);
+	}
+
 	@ResponseBody
 	@RequestMapping(value="/json", method=RequestMethod.GET)
-	public JsonResult json() {
+	public Object json() {
 		GuestbookVo vo = new GuestbookVo();
 		vo.setNo(1L);
 		vo.setName("둘리");
