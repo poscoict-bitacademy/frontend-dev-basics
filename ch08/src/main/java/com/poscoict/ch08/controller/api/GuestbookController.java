636e32ab50a90fd1abce9a782005f3a53f3a9cb8
poscoict-bitacademy <poscoict.bitacademy@gmail.com>
2022-03-02 14:46:22 +0900

    guestbook spa #2

--- ch08/src/main/java/com/poscoict/ch08/controller/api/GuestbookController.java
+++ ch08/src/main/java/com/poscoict/ch08/controller/api/GuestbookController.java
@@ -1,9 +1,13 @@
 package com.poscoict.ch08.controller.api;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.poscoict.ch08.controller.dto.JsonResult;
@@ -20,6 +24,34 @@ public class GuestbookController {
 		vo.setNo(1L);
 		vo.setPassword("");
 		return JsonResult.success(vo);
+	}
+
+	@ResponseBody
+	@RequestMapping(value="/list", method=RequestMethod.GET)
+	public JsonResult ex01(@RequestParam(value="sn", required=true, defaultValue="-1") Long no) {
+		// guestbookService.getMessageList(no);
+		
+		List<GuestbookVo> list = new ArrayList<>();
+		
+		GuestbookVo vo1 = new GuestbookVo();
+		vo1.setNo(3L);
+		vo1.setName("둘리3");
+		vo1.setMessage("안녕3");
+		list.add(vo1);
+		
+		GuestbookVo vo2 = new GuestbookVo();
+		vo2.setNo(2L);
+		vo2.setName("둘리2");
+		vo2.setMessage("안녕2");
+		list.add(vo2);
+		
+		GuestbookVo vo3 = new GuestbookVo();
+		vo3.setNo(1L);
+		vo3.setName("둘리1");
+		vo3.setMessage("안녕1");
+		list.add(vo3);
+		
+		return JsonResult.success(list);
 	}
 	
 }
--- ch08/src/main/webapp/WEB-INF/views/gb/ex01.jsp
+++ ch08/src/main/webapp/WEB-INF/views/gb/ex01.jsp
@@ -63,34 +63,34 @@ $(function() {
 				</form>
 				<ul id="list-guestbook">
 					
-					<li data-no=''>
+					<li data-no='14'>
 						<strong>지나가다가</strong>
 						<p>
 							별루입니다.<br>
 							비번:1234 -,.-
 						</p>
 						<strong></strong>
-						<a href='' data-no=''>삭제</a> 
+						<a href='' data-no='14'>삭제</a> 
 					</li>
 					
-					<li data-no=''>
+					<li data-no='13'>
 						<strong>둘리</strong>
 						<p>
 							안녕하세요<br>
 							홈페이지가 개 굿 입니다.
 						</p>
 						<strong></strong>
-						<a href='' data-no=''>삭제</a> 
+						<a href='' data-no='13'>삭제</a> 
 					</li>
 
-					<li data-no=''>
+					<li data-no='12'>
 						<strong>주인</strong>
 						<p>
 							아작스 방명록 입니다.<br>
 							테스트~
 						</p>
 						<strong></strong>
-						<a href='' data-no=''>삭제</a> 
+						<a href='' data-no='12'>삭제</a> 
 					</li>
 					
 									
